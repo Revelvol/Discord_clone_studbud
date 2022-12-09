feec6ae9b2dd1e8d35465685195e7f9368e62bb4
Revelvol <111274957+Revelvol@users.noreply.github.com>
2022-12-09 22:26:45 +0700

    Update settings.py

--- studbud/settings.py
+++ studbud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-po4sp*06#ntsg&!m_q_vzx=&!py5@69$7zv*)kt*l8=#d2c*ka'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
